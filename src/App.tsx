2fe2264cdea0e95dc8695c2a7acd85e8c55ee00f
christophermarek <christophermarek42@gmail.com>
2021-06-28 14:32:40 -0400

    barebones for login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [formToDisplay, setFormToDisplay] = useState<string>('register');
+
+  const [userName, setUserName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const registerClicked = () => {
+    //do nothing
+  }
+
+  
+  const loginClicked = () => {
+    //do nothing
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{isLoggedIn ? 'Logged in' : 'Not Logged in'}</p>
+      <p>Exchange Name</p>
+      <p>We will never track your data</p>
+      {isLoggedIn ?
+        (
+          <input type='button' value='Logout' />
+        )
+        :
+        (
+          <div className='formSelect'>
+            <input type='button' value={'Login'} onClick={() => setFormToDisplay('login')} />
+            <input type='button' value={'Register'} onClick={() => setFormToDisplay('register')} />
+            {/* Need to separate into components for private state later but right now they are the same fields*/}
+            {formToDisplay === 'register' ?
+              (
+                <div className='register'>
+                  <p>Register</p>
+
+                  <p>Username</p><input type='text' value={userName} onChange={(event) => setUserName(event.target.value)} />
+                  <p>Password</p><input type='text' value={password} onChange={(event) => setPassword(event.target.value)} />
+                  <input type="button" value='Register' onClick={registerClicked}/>
+                </div>
+              )
+              :
+              (
+                <div className='login'>
+                  <p>Login</p>
+
+                  <p>Username</p><input type='text' value={userName} onChange={(event) => setUserName(event.target.value)} />
+                  <p>Password</p><input type='text' value={password} onChange={(event) => setPassword(event.target.value)} />
+                  <input type="button" value='Login' onClick={loginClicked}/>
+                </div>
+              )
+            }
+
+          </div>
+        )}
     </div>
   );
 }
